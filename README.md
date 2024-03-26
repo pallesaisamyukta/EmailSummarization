9f87a4f15ad038d10ce375a811fda937960c9a19
rootsec1 <nateriver210@gmail.com>
2024-03-26 01:57:43 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # EmailSummarization
+
+## Steps to run the API server
+
+```
+cd EmailSummarization
+pip install -r requirements.txt
+uvicorn --backend.server:app
+```
+
+## Loading the Extension into Chrome
+
+- Download and Unzip the Extension:
+  First, clone this repository or download the zip file of the extension to your local machine. If downloaded as a zip, extract the contents to a folder of your choice.
+
+- Open Chrome Extensions Page:
+  Open the Google Chrome browser and navigate to the extensions page by entering chrome://extensions/ in the address bar. Alternatively, you can access this page by clicking on the three dots at the top-right corner of the browser, selecting 'More tools', and then selecting 'Extensions'.
+
+- Enable Developer Mode:
+  Toggle the 'Developer mode' switch on the top-right corner of the extensions page. This allows you to load unpacked extensions.
+
+- Load the Unpacked Extension:
+  Click on the 'Load unpacked' button which appears after enabling Developer Mode. Navigate to the folder where you extracted the extension files and select it. Ensure that you select the folder containing the manifest.json file.
+
+- Verify Installation:
+  After selecting the folder, the extension should now appear in your list of installed extensions. Ensure there are no errors and that the extension is enabled.
--- backend/services/mailer.py
+++ backend/services/mailer.py
@@ -23,7 +23,6 @@ def clean_and_extract_text(html_list):
         # Replace multiple whitespaces with a single space
         text = re.sub(r'\s+', ' ', text)
         text = text.strip()
-        text = text.lower()
         cleaned_texts.append(text)
     return cleaned_texts
 
@@ -57,7 +56,7 @@ class EmailTLDR:
         self.mail.login(self.email_address, self.email_password)
         self.mail.select('inbox')
 
-    def fetch_emails_since(self, days_ago=1):
+    def fetch_emails_since(self, days_ago=7):
         """
         Fetches emails from the last specified number of days.
 
@@ -132,33 +131,45 @@ class EmailTLDR:
         Returns:
         - A string containing the summary of the emails.
         """
-        # Clean and extract text from the HTML strings
-        # email_bodies = email_bodies[:5]
-        extracted_texts = clean_and_extract_text(email_bodies)
+        summary_list = []
+        for email_body in email_bodies:
+            if email_body == "":
+                continue
 
-        prompt = ("")
-        # Limit to first 5 for brevity
-        prompt += " : " + " ".join(extracted_texts)
-        print(prompt)
-        # Specify to load the model to CPU
-        model = BartForConditionalGeneration.from_pretrained(
-            'facebook/bart-base',
-            state_dict=torch.load(model_path, map_location=torch.device('cpu'))
-        )
-        tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
-        inputs = tokenizer(
-            [prompt],
-            return_tensors="pt",
-            max_length=1024,
-            truncation=True
-        )
-        summary_ids = model.generate(
-            inputs['input_ids'],
-            num_beams=4,
-            max_length=200,
-            early_stopping=True
-        )
-        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+            # Clean and extract text from the HTML strings
+            extracted_texts = clean_and_extract_text([email_body])
+
+            prompt = ("")
+            # Limit to first 5 for brevity
+            prompt += " : " + " ".join(extracted_texts)
+            print(prompt)
+            # Specify to load the model to CPU
+            model = BartForConditionalGeneration.from_pretrained(
+                'facebook/bart-base',
+                state_dict=torch.load(
+                    model_path, map_location=torch.device('cpu'))
+            )
+            tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
+            inputs = tokenizer(
+                [prompt],
+                return_tensors="pt",
+                max_length=1024,
+                truncation=True
+            )
+            summary_ids = model.generate(
+                inputs['input_ids'],
+                num_beams=4,
+                max_length=200,
+                early_stopping=True
+            )
+            summary = tokenizer.decode(
+                summary_ids[0],
+                skip_special_tokens=True
+            )
+            summary = str(summary).replace("\n", " ").strip()
+            summary_list.append(summary)
+
+        summary = "\n\n".join(summary_list)
         return summary
 
     def send_email(self, body):
@@ -168,11 +179,19 @@ class EmailTLDR:
         Parameters:
         - body: The body of the email to be sent.
         """
-        message = MIMEMultipart()
+        # Split the body text by newlines, strip each line, and wrap it with <li> tags
+        body_lines = body.split('\n')
+        body_items = ['<li>{}</li>'.format(line.strip())
+                      for line in body_lines if line.strip() != '']
+        body_html = '<ul>{}</ul>'.format(''.join(body_items))
+
+        # 'alternative' for HTML and plain text
+        message = MIMEMultipart('alternative')
         message['From'] = self.sender_email
         message['To'] = self.recipient_email
         message['Subject'] = "Your Weekly Email-TLDR!"
-        message.attach(MIMEText(body, 'plain'))
+        message.attach(MIMEText(body, 'plain'))  # Attach plain text version
+        message.attach(MIMEText(body_html, 'html'))  # Attach HTML version
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
